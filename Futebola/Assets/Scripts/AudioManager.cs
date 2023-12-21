c74fe924e553052dba84a272aa7f08eaef1544a5
Diogo Benevenuto <diogommonteiro123@gmail.com>
2023-12-21 00:57:38 -0300

    Create AudioManager

---
+++ Futebola/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] clips;
+    public AudioSource musicaBG;
+
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad (this.gameObject);
+        }
+        else
+        {
+            Destroy (gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(!musicaBG.isPlaying)
+        {
+            musicaBG.clip = GetRandom();
+            musicaBG.Play ();
+        }
+    }
+
+    AudioClip GetRandom()
+    {
+        return clips [Random.Range(0, clips.Length)];
+    }
+
+
+}
